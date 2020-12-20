3cfb8783437c1efe1e54ace4afdbd510414a1e45
Ola <olakas99@wp.pl>
2020-12-20 23:54:04 +0100

    zadanie_7

---
+++ app/src/main/java/pl/edu/pb/libraryapp/Book.java
@@ -0,0 +1,41 @@
+package pl.edu.pb.libraryapp;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "book")
+public class Book {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
